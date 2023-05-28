03a4d68e9df028fd8019c498d706e299f3b13304
21522320 <100219569+21522320@users.noreply.github.com>
2023-05-28 21:19:29 +0700

    Add files via upload

---
+++ tables/ACCOUNTS.sql
@@ -0,0 +1,56 @@
+USE SAVINGS
+GO
+
+CREATE TABLE AccountTypes( -- NHOMNGUOIDUNG
+	AccountTypeID INT,
+	AccountTypeName VARCHAR(20),
+);
+
+ALTER TABLE AccountTypes 
+ADD CONSTRAINT PK_AccountTypes 
+PRIMARY KEY(AccountTypeID); 
+
+
+CREATE TABLE Accounts( -- NGUOIDUNG
+	AccountID INT,
+	Username VARCHAR(20),
+	AccountTypeID VARCHAR(20),
+	AccountPasword VARCHAR(50),
+);
+
+ALTER TABLE Accounts 
+ADD CONSTRAINT PK_Accounts 
+PRIMARY KEY(AccountID); 
+
+ALTER TABLE Accounts 
+ADD CONSTRAINT FK_Accounts_AccountTypes 
+FOREIGN KEY (AccountTypeID) REFERENCES AccountTypes(AccountTypeID); 
+
+
+CREATE TABLE UserFunctionality( -- CHUCNANG
+	UserFunctionalityID INT,
+	UserFunctionalityName VARCHAR(20),
+	UFNDescription VARCHAR(100),
+);
+
+ALTER TABLE UserFunctionality 
+ADD CONSTRAINT PK_UserFunctionality 
+PRIMARY KEY(UserFunctionalityID);
+
+
+CREATE TABLE UserAuthorization( -- PHANQUYEN
+	AccountTypeID INT,
+	UserFunctionalityID INT,
+);
+
+ALTER TABLE UserAuthorization 
+ADD CONSTRAINT PK_AccountTypes_UserFunctionality 
+PRIMARY KEY(AccountTypeID, UserFunctionalityID);
+
+ALTER TABLE UserAuthorization 
+ADD CONSTRAINT FK_UserAuthorization_AccountTypes 
+FOREIGN KEY (AccountTypeID) REFERENCES AccountTypes(AccountTypeID);
+
+ALTER TABLE UserAuthorization 
+ADD CONSTRAINT FK_UserAuthorization_UserFunctionality 
+FOREIGN KEY (UserFunctionalityID) REFERENCES UserFunctionality(UserFunctionalityID);
