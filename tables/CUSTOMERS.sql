648b59b0447838b1872a83d3c9aaadf934b3ad5d
21522320 <100219569+21522320@users.noreply.github.com>
2023-05-28 20:34:49 +0700

    Add files via upload

---
+++ tables/CUSTOMERS.sql
@@ -0,0 +1,15 @@
+USE SAVINGS
+GO
+
+-- CREATE CUSTOMER TABLE
+-- Creat Customer table
+CREATE TABLE Customers
+(
+	CustomerID 	CHAR(10),
+	CustomerName	VARCHAR(40),
+	PhoneNumber	VARCHAR(20),
+	CitizenID	VARCHAR(20),
+	CustomerAddress	VARCHAR(100),
+)
+-- Add primary key
+ALTER TABLE Customers ADD CONSTRAINT PK_Customer PRIMARY KEY (CustomerID)
